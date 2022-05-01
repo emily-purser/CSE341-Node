492f891f3694bc7f02e35b8296a1f73b20d6f3b4
Emily Purser <83991483+emily-purser@users.noreply.github.com>
2022-04-30 18:09:09 -0600

    attempt to fix heroku 7

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require("./db/connection");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.use(bodyParser.json())
 .use((req, res, next) => {
