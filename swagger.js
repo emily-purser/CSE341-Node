72ed0f06c01649f374b69662b99b678527ce69a2
Emily Purser <83991483+emily-purser@users.noreply.github.com>
2022-05-11 11:50:47 -0600

    attempt 2 to get api on heroku

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Contacts API'
     },
     host: 'cse341-emily-purser.herokuapp.com',
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
