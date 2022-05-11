dee90d2e080943670aea6acf9b4fdc8cbd1989e2
Emily Purser <83991483+emily-purser@users.noreply.github.com>
2022-05-11 11:19:51 -0600

    Worked to get Swagger API connected

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) =>{
-    res.send('Kelli Purser');
-});
-
+// routes.get('/', (req, res) =>{
+//     res.send('Kelli Purser');
+// });
+routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts'));
 module.exports = routes;
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'My API',
+        description: 'Contacts API'
+    },
+    host: 'cse341-emily-purser.herokuapp.com',
+    schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpoint = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpoint, doc);
